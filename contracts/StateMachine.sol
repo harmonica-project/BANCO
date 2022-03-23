107f76083ad3c8bd489e6b0b7dd29778ff1acf7a
nicoSix <nicolas.six.fr@gmail.com>
2022-03-23 16:55:58 +0100

    Formatting + fixing vuln in two functions

--- contracts/StateMachine.sol
+++ contracts/StateMachine.sol
@@ -2,121 +2,116 @@
 pragma solidity >0.8.0;
 
 contract StateMachine {
-    // CONTRACT VARIABLES
+  // CONTRACT VARIABLES
 
-    struct Transition {
-        string name;
-        string previousState;
-        string nextState;
-        string[] authorized;
-        bool exists;
+  struct Transition {
+    string name;
+    string previousState;
+    string nextState;
+    string[] authorized;
+    bool exists;
+  }
+
+  struct Participant {
+    string name;
+    address addr;
+  }
+
+  mapping(string => Transition) transitions;
+  mapping(string => address) participants;
+  mapping(string => bool) existingStates;
+  string currentState;
+
+  // CONSTRUCTOR
+  constructor(
+    string memory _startState,
+    string[] memory _states,
+    Transition[] memory _transitions,
+    Participant[] memory _participants
+  ) {
+    for (uint256 i = 0; i < _states.length; i++) {
+      existingStates[_states[i]] = true;
     }
 
-    struct Participant {
-        string name;
-        address addr;
+    // Add if participants are statically added
+    for (uint256 i = 0; i < _participants.length; i++) {
+      participants[_participants[i].name] = _participants[i].addr;
     }
 
-    mapping(string => Transition) transitions;
-    mapping(string => address) participants;
-    mapping(string => bool) existingStates;
-    string currentState;
+    require(existingStates[_startState], "Start state does not exist.");
+    currentState = _startState;
 
-    // CONSTRUCTOR
-    constructor(
-        string memory _startState,
-        string[] memory _states,
-        Transition[] memory _transitions,
-        Participant[] memory _participants
-    ) {
-        for (uint256 i = 0; i < _states.length; i++) {
-            existingStates[_states[i]] = true;
-        }
+    for (uint256 i = 0; i < _transitions.length; i++) {
+      require(
+        existingStates[_transitions[i].previousState],
+        "previousState of transition does not exist."
+      );
+      require(
+        existingStates[_transitions[i].nextState],
+        "nextState of transition does not exist."
+      );
+      require(
+        !transitions[_transitions[i].name].exists,
+        "Transition duplicate."
+      );
+      require(
+        checkAuthorized(_transitions[i].authorized),
+        "Transaction participant do not exist or no participant specified."
+      );
+      require(
+        _transitions[i].exists,
+        "Transaction exists field must be set to true."
+      );
+      transitions[_transitions[i].name] = _transitions[i];
+    }
+  }
 
-        // Add if participants are statically added
-        for (uint256 i = 0; i < _participants.length; i++) {
-            participants[_participants[i].name] = _participants[i].addr;
-        }
+  // MODIFIERS
 
-        require(existingStates[_startState], "Start state does not exist.");
-        currentState = _startState;
+  modifier isAuthorized(string memory _txname) {
+    bool found;
 
-        for (uint256 i = 0; i < _transitions.length; i++) {
-            require(
-                existingStates[_transitions[i].previousState],
-                "previousState of transition does not exist."
-            );
-            require(
-                existingStates[_transitions[i].nextState],
-                "nextState of transition does not exist."
-            );
-            require(
-                !transitions[_transitions[i].name].exists,
-                "Transition duplicate."
-            );
-            require(
-                checkAuthorized(_transitions[i].authorized),
-                "Transaction participant do not exist or no participant specified."
-            );
-            require(
-                _transitions[i].exists,
-                "Transaction exists field must be set to true."
-            );
-            transitions[_transitions[i].name] = _transitions[i];
-        }
+    for (uint256 i = 0; i < transitions[_txname].authorized.length; i++) {
+      if (participants[transitions[_txname].authorized[i]] == msg.sender) {
+        found = true;
+        break;
+      }
     }
 
-    // MODIFIERS
+    require(found, "Participant not authorized.");
+    _;
+  }
 
-    modifier isAuthorized(string memory _txname) {
-        bool found;
+  // FUNCTIONS
 
-        for (uint256 i = 0; i < transitions[_txname].authorized.length; i++) {
-            if (
-                participants[transitions[_txname].authorized[i]] == msg.sender
-            ) {
-                found = true;
-                break;
-            }
-        }
+  function checkAuthorized(string[] memory authorized)
+    private
+    pure
+    returns (bool)
+  {
+    // checks if all participants authorized exists, and if they are at least one
+    return (authorized.length > 0);
+  }
 
-        require(found, "Participant not authorized.");
-        _;
-    }
+  function fireTransition(string memory _txname) public isAuthorized(_txname) {
+    require(transitions[_txname].exists, "Transition does not exist.");
+    require(
+      keccak256(abi.encode(transitions[_txname].previousState)) ==
+        keccak256(abi.encode(currentState)),
+      "Current state does not allow firing this transition."
+    );
+    currentState = transitions[_txname].nextState;
+  }
 
-    // FUNCTIONS
+  function getCurrentState() public view returns (string memory) {
+    return currentState;
+  }
 
-    function checkAuthorized(string[] memory authorized)
-        private
-        pure
-        returns (bool)
-    {
-        // checks if all participants authorized exists, and if they are at least one
-        return (authorized.length > 0);
-    }
-
-    function fireTransition(string memory _txname)
-        public
-        isAuthorized(_txname)
-    {
-        require(transitions[_txname].exists, "Transition does not exist.");
-        require(
-            keccak256(abi.encode(transitions[_txname].previousState)) ==
-                keccak256(abi.encode(currentState)),
-            "Current state does not allow firing this transition."
-        );
-        currentState = transitions[_txname].nextState;
-    }
-
-    function getCurrentState() public view returns (string memory) {
-        return currentState;
-    }
-
-    function getParticipantAddr(string memory _name)
-        public
-        view
-        returns (address)
-    {
-        return participants[_name];
-    }
+  function getParticipantAddr(string memory _name)
+    public
+    view
+    returns (address)
+  {
+    return participants[_name];
+  }
 }
