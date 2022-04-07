664bec8dfbaf0dfe48489e138606b6d98c7590fa
nicoSix <nicolas.six.fr@gmail.com>
2022-04-07 14:57:55 +0200

    Adding role selector

--- src/assets/app/src/components/Setup/RoleSetupForm.jsx
+++ src/assets/app/src/components/Setup/RoleSetupForm.jsx
@@ -11,8 +11,15 @@ import {
   IconButton,
   FormGroup,
   FormControlLabel,
-  Checkbox
+  Checkbox,
+  FormControl,
+  InputLabel,
+  Select,
+  OutlinedInput,
+  Chip,
+  MenuItem
 } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import ClearIcon from '@mui/icons-material/Clear';
 import AddIcon from '@mui/icons-material/Add';
 import { makeStyles } from '@mui/styles';
@@ -35,12 +42,35 @@ const deepCopy = (item) => JSON.parse(JSON.stringify(item));
 
 const blankRole = {
   name: '',
-  isAdmin: false
+  isAdmin: false,
+  managedRoles: []
+}
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
 }
 
 const RoleSetupForm = ({ nextPage }) => {
   const classes = useStyles();
+  const theme = useTheme();
   const [roles, setRoles] = useState([blankRole]);
+  const [personName, setPersonName] = React.useState([]);
 
   const rolesValid = () => {
     const foundRoles = {};
@@ -61,6 +91,12 @@ const RoleSetupForm = ({ nextPage }) => {
   const changeRoleName = (roleId, e) => {
     const newRoles = deepCopy(roles);
     newRoles[roleId].name = e.target.value;
+    setRoles(newRoles);
+  };
+
+  const changeManagedRoles = (roleId, e) => {
+    const newRoles = deepCopy(roles);
+    newRoles[roleId].managedRoles = e.target.value;
     setRoles(newRoles);
   };
 
@@ -130,30 +166,31 @@ const RoleSetupForm = ({ nextPage }) => {
                             label="Admin"
                           />
                           <FormControl sx={{ m: 1, width: 300 }}>
-                            <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
+                            <InputLabel id="demo-multiple-chip-label">Managed Roles</InputLabel>
                             <Select
                               labelId="demo-multiple-chip-label"
                               id="demo-multiple-chip"
                               multiple
-                              value={personName}
-                              onChange={handleChange}
+                              value={roles[i].managedRoles}
+                              onChange={changeManagedRoles.bind(this, i)}
                               input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                              renderValue={(selected) => (
+                              renderValue={(managedRoles) => (
                                 <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                                  {selected.map((value) => (
-                                    <Chip key={value} label={value} />
-                                  ))}
+                                  {managedRoles.map((value) => {
+                                    console.log(value)
+                                    return <Chip key={value} label={value} />
+                                  })}
                                 </Box>
                               )}
                               MenuProps={MenuProps}
                             >
-                              {names.map((name) => (
+                              {roles.map((r) => (
                                 <MenuItem
-                                  key={name}
-                                  value={name}
-                                  style={getStyles(name, personName, theme)}
+                                  key={r.name}
+                                  value={r.name}
+                                  style={getStyles(r.name, personName, theme)}
                                 >
-                                  {name}
+                                  {r.name}
                                 </MenuItem>
                               ))}
                             </Select>
