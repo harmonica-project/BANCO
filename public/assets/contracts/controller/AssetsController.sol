04bb704dd53c701c298b01214c84d79d55a941ec
nicoSix <nicolasix@outlook.fr>
2022-04-19 00:12:39 +0200

    Adding dynamic key/value array to attach data to assets

--- public/assets/contracts/controller/AssetsController.sol
+++ public/assets/contracts/controller/AssetsController.sol
@@ -13,7 +13,9 @@ contract AssetsController {
     /* #EventsEmission */
     // ---- EVENTS ---- //
     event NewOwner(string indexed assetName, address indexed newOwner);
-    event NewAsset(Assets.Asset indexed  asset);
+    event NewAsset(Assets.Asset indexed asset);
+    event NewAttachedData(Assets.Asset indexed asset, string indexed key, string indexed value);
+    event NewDetachedData(Assets.Asset indexed asset, string indexed key);
     /* /EventsEmission */
 
     constructor(
@@ -46,12 +48,38 @@ contract AssetsController {
         /* /EventsEmission */
     }
 
-    function newAsset(string memory _assetName, bytes32 _assetDesc) public {
+    function newAsset(string memory _assetName, bytes32 _assetDesc, string[2][] memory _attachedData) public {
         require(participantsContract.doesParticipantExist(msg.sender), "Participant does not exist.");
 
-        assetsContract.newAsset(_assetName, _assetDesc, msg.sender);
+        assetsContract.newAsset(_assetName, _assetDesc, msg.sender, _attachedData);
         /* #EventsEmission */
         emit NewAsset(assetsContract.getAsset(_assetName));
         /* /EventsEmission */
+    }
+
+    function attachData(string memory _assetName, string memory _key, string memory _value) public {
+        address currentOwner = assetsContract.getAsset(_assetName).owner;
+        require(currentOwner == msg.sender, "Sender is not the owner of the asset.");
+
+        assetsContract.attachData(_assetName, _key, _value);
+
+        /* #EventsEmission */
+        emit NewAttachedData(assetsContract.getAsset(_assetName), _key, _value);
+        /* /EventsEmission */
+    }
+
+    function detachData(string memory _assetName, string memory _key) public {
+        address currentOwner = assetsContract.getAsset(_assetName).owner;
+        require(currentOwner == msg.sender, "Sender is not the owner of the asset.");
+
+        assetsContract.detachData(_assetName, _key);
+
+        /* #EventsEmission */
+        emit NewDetachedData(assetsContract.getAsset(_assetName), _key);
+        /* /EventsEmission */
+    }
+
+    function getAttachedData(string memory _assetName, string memory _key) public view returns (string memory) {
+        return assetsContract.getAttachedData(_assetName, _key);
     }
 }
--- public/assets/contracts/data/Assets.sol
+++ public/assets/contracts/data/Assets.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >0.8.0;
 
+import "../lib/Helpers.sol";
+
 contract Assets {
     // ---- STRUCTS ---- //
     struct Asset {
@@ -8,6 +10,7 @@ contract Assets {
         string name;
         bytes32 desc;
         bool exists;
+        string[2][] attachedData;
     }
 
     struct AssetTransferRecord {
@@ -63,15 +66,54 @@ contract Assets {
         assetsHistories[_assetName].push(AssetTransferRecord(block.timestamp, previousOwner, _newOwner));
     }
 
-    function newAsset(string memory _assetName, bytes32 _assetDesc, address _owner) public onlyController {
+    function newAsset(string memory _assetName, bytes32 _assetDesc, address _owner, string[2][] memory _attachedData) public onlyController {
         require(!assets[_assetName].exists, "Asset already exists.");
 
-        assets[_assetName] = Asset(_owner, _assetName, _assetDesc, true);
+        assets[_assetName] = Asset(_owner, _assetName, _assetDesc, true, _attachedData);
     }
 
     function getAsset(string memory _assetName) public view returns (Asset memory) {
         require(assets[_assetName].exists, "Asset does not exist.");
 
         return assets[_assetName];
+    }
+
+    function attachData(string memory _assetName, string memory _key, string memory _value) public {
+        require(assets[_assetName].exists, "Asset does not exist.");
+
+        for (uint i = 0; i < assets[_assetName].attachedData.length; i++) {
+            if (Helpers.strCmp(assets[_assetName].attachedData[i][0], _key)) {
+                assets[_assetName].attachedData[i][1] = _value;
+                return;
+            }
+        }
+
+        assets[_assetName].attachedData.push([_key, _value]);
+    }
+
+    function detachData(string memory _assetName, string memory _key) public {
+        require(assets[_assetName].exists, "Asset does not exist.");
+
+        for (uint i = 0; i < assets[_assetName].attachedData.length; i++) {
+            if (Helpers.strCmp(assets[_assetName].attachedData[i][0], _key)) {
+                // moving the last item of the array to the item to delete then pop the old one at the end of the array
+                assets[_assetName].attachedData[i] = assets[_assetName].attachedData[assets[_assetName].attachedData.length - 1];
+                assets[_assetName].attachedData.pop();
+            }
+        }
+
+        revert("Key not found in array.");
+    }
+
+    function getAttachedData(string memory _assetName, string memory _key) public view returns (string memory) {
+        require(assets[_assetName].exists, "Asset does not exist.");
+
+        for (uint i = 0; i < assets[_assetName].attachedData.length; i++) {
+            if (Helpers.strCmp(assets[_assetName].attachedData[i][0], _key)) {
+                return assets[_assetName].attachedData[i][1];
+            }
+        }
+
+        revert("Key not found in array.");
     }
 }
